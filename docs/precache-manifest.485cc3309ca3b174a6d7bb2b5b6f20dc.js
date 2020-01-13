9af3ff49fa10bf8f4d4a87dc2bb74402513b697f
slizbeth <stefany.lizbeth@gmail.com>
2020-01-13 13:10:23 +0100

    add docs

---
+++ docs/precache-manifest.485cc3309ca3b174a6d7bb2b5b6f20dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "728b8ff12b1915d12defe6482ca76231",
+    "url": "./index.html"
+  },
+  {
+    "revision": "29b964fbb8e776e51102",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "50552f6d008ba600dece",
+    "url": "./static/js/2.68ecea0e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.68ecea0e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "29b964fbb8e776e51102",
+    "url": "./static/js/main.38ecbfeb.chunk.js"
+  },
+  {
+    "revision": "6e8cb7e42df797c023ed",
+    "url": "./static/js/runtime-main.be7b2e22.js"
+  }
+]);
---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../stylesheets/App.css';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        <h1>react-example</h1>
+        <iframe src="https://giphy.com/embed/Wj7lNjMNDxSmc" width="480" height="349" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/dog-miss-Wj7lNjMNDxSmc">via GIPHY</a></p>
+      </div>
+    );
+  }
+}
+
+export default App;
